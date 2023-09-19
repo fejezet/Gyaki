7124494fc9a173563d3d5dda26c44d06f4245b08
fejezet <fiatalfejezet@gmail.com>
2023-09-19 18:27:03 +0200

    start

---
+++ Gyaki3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyaki3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Gyaki 3 Start ->2022 Emelt Infó érettségi progizásból
+        }
+    }
+}

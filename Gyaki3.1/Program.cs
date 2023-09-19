d35f8cf3dad4fc45d3ac1d4df1f66fd614eba3c3
fejezet <fiatalfejezet@gmail.com>
2023-09-19 19:45:27 +0200

    Gyaki 3

---
+++ Gyaki3.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyaki3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Gyaki 3
+        }
+    }
+}
